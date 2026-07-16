8b4ee7f0d9d102cd90514e7c877fb396de8fed31
aman <amankhan1017@gmail.com>
2026-07-16 17:46:02 +0500

    scenario added to refactor code

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -3,11 +3,13 @@ mod engine;
 mod evaluation;
 mod intent;
 mod rules;
+mod scenario;
 mod validators;
 
 pub use decision::Decision;
 pub use engine::evaluate;
 pub use evaluation::{Evaluation, RuleHit};
 pub use intent::{ActionType, Intent};
+pub use scenario::Scenario;
 
 pub(crate) use engine::hit;
